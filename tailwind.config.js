c43867cfc865b09910b4bed2542af5e5e44bfb3a
EdsonAmaya7 <edson_amaya99@live.com>
2022-07-08 19:09:48 -0500

    a

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,13 @@ module.exports = {
         './resources/views/**/*.blade.php',
     ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-            },
-        },
-    },
+    // theme: {
+    //     extend: {
+    //         fontFamily: {
+    //             sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+    //         },
+    //     },
+    // },
 
     plugins: [require('@tailwindcss/forms')],
 };
